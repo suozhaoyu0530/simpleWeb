b31b60191bc91cf9ce0f4ab79aa6a6fcb7506da4
suozhaoyu0530 <suozhaoyu0530@163.com>
2017-09-19 14:31:48 +0800

    将controller异常处理的方法拆为三个方法，一个负责转发处理，另外两个负责针对不同的返回值做不同的逻辑处理

--- src/main/util/com/xiaosuo/aspectJ/impl/base/AbstractControllerEventImpl.java
+++ src/main/util/com/xiaosuo/aspectJ/impl/base/AbstractControllerEventImpl.java
@@ -29,41 +29,61 @@ public abstract class AbstractControllerEventImpl extends AbstractEventImpl{
 	@Override
 	public Object dealReturnValue(Exception e, String returnType, Object[] params) {
 		if("java.lang.String".equals(returnType)){
-			Optional<Object> requestOp = Stream.of(params)
-					.filter(inParam -> inParam instanceof HttpServletRequest)
-					.findFirst();
-			if(!requestOp.isPresent()){
-				throw new UtilException("未获得入参中的request对象");
-			}
-			HttpServletRequest request = (HttpServletRequest) requestOp.get();
-			String message = super.defaultReturnStr(e);
-			request.setAttribute("msg", message);
-			return "error";
+			return doReturnStringType(e, params);
 		}
 		
 		if("void".equals(returnType)){
-			Optional<Object> responseOp = Stream.of(params).filter(inParam -> inParam instanceof HttpServletResponse).findFirst();
-			if(!responseOp.isPresent()){
-				throw new UtilException("未获得入参中的response对象");
-			}
-			HttpServletResponse response = (HttpServletResponse) responseOp.get();
-			Map<String, Object> resultM = super.defaultReturnMap(e);
-			JSONObject json = new JSONObject(resultM);
-			
-			PrintWriter out = null;
-			try {
-				out = response.getWriter();
-				out.write(json.toString());
-				out.flush();
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}finally {
-				out.close();
-			}
-			
+			doReturnVoidType(e, params);
 			return null;
 		}
 		
 		return super.dealReturnValue(e, returnType, params);
+	}
+	
+	/**
+	 * 处理返回值为String的数据
+	 * 
+	 * @param e
+	 * @param params
+	 * @return
+	 */
+	protected Object doReturnStringType(Exception e, Object[] params){
+		Optional<Object> requestOp = Stream.of(params)
+				.filter(inParam -> inParam instanceof HttpServletRequest)
+				.findFirst();
+		if(!requestOp.isPresent()){
+			throw new UtilException("未获得入参中的request对象");
+		}
+		HttpServletRequest request = (HttpServletRequest) requestOp.get();
+		String message = super.defaultReturnStr(e);
+		request.setAttribute("msg", message);
+		return "error";
+	}
+	
+	/**
+	 * 处理返回值为void的数据
+	 * 
+	 * @param e
+	 * @param params
+	 */
+	protected void doReturnVoidType(Exception e, Object[] params){
+		Optional<Object> responseOp = Stream.of(params).filter(inParam -> inParam instanceof HttpServletResponse).findFirst();
+		if(!responseOp.isPresent()){
+			throw new UtilException("未获得入参中的response对象");
+		}
+		HttpServletResponse response = (HttpServletResponse) responseOp.get();
+		Map<String, Object> resultM = super.defaultReturnMap(e);
+		JSONObject json = new JSONObject(resultM);
+		
+		PrintWriter out = null;
+		try {
+			out = response.getWriter();
+			out.write(json.toString());
+			out.flush();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}finally {
+			out.close();
+		}
 	}
 }
